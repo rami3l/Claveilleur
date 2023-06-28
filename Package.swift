6669f6c716031e058b2ad8f0c6bc48bdcfb5f258
rami3l <rami3l@outlook.com>
2023-06-28 20:47:26 +0800

    feat: impl `currentAppObserver`

--- Package.swift
+++ Package.swift
@@ -4,12 +4,15 @@
 import PackageDescription
 
 let package = Package(
-    name: "Claveilleur",
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .executableTarget(
-            name: "Claveilleur",
-            path: "Sources"),
-    ]
+  name: "Claveilleur",
+  platforms: [
+    .macOS(.v10_15)
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package, defining a module or a test suite.
+    // Targets can depend on other targets in this package and products from dependencies.
+    .executableTarget(
+      name: "Claveilleur",
+      path: "Sources")
+  ]
 )
--- Sources/main.swift
+++ Sources/main.swift
@@ -1,4 +1,21 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import AppKit
 
-print("Hello, world!")
+let currentAppObserver = NSWorkspace
+  .shared
+  .notificationCenter
+  .publisher(for: NSWorkspace.didActivateApplicationNotification)
+  .sink { notification in
+    guard
+      let info =
+        notification.userInfo?[NSWorkspace.applicationUserInfoKey]
+        as? NSRunningApplication,
+      let identifier = info.bundleIdentifier
+    else {
+      return
+    }
+
+    // TODO: Remove this
+    print("Switching to app: \(identifier)")
+  }
+
+CFRunLoopRun()
