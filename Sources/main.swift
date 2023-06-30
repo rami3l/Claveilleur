402506d350228362138739c27b0e5898ad5f465b
rami3l <rami3l@outlook.com>
2023-06-30 22:23:08 +0800

    feat: impl `currentInputSourceObserver`

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,6 +1,19 @@
 import AppKit
 import Carbon
 
+// TODO: Use Apple unified logging to replace `print`s: https://github.com/chrisaljoudi/swift-log-oslog
+
+// TODO: Add CLI interface: launch (normal), --(un)?install-service, --(start|stop|restart)-service
+
+// TODO: Handle spotlight: https://stackoverflow.com/questions/36264038/cocoa-programmatically-detect-frontmost-floating-windows
+
+let suiteName = "io.github.rami3l.Claveilleur"
+let userDefaults = UserDefaults(suiteName: suiteName)!
+
+func saveInputSource(_ id: String, forApp appID: String) {
+  userDefaults.set(id, forKey: appID)
+}
+
 // https://github.com/mzp/EmojiIM/issues/27#issue-1361876711
 func getInputSource() -> String {
   let inputSource = TISCopyCurrentKeyboardInputSource().takeUnretainedValue()
@@ -11,34 +24,60 @@ func getInputSource() -> String {
 }
 
 // https://github.com/daipeihust/im-select/blob/83046bb75333e58c9a7cbfbd055db6f360361781/macOS/im-select/im-select/main.m
-func setInputSource(to id: String) {
+func setInputSource(to id: String) -> Bool {
+  if getInputSource() == id {
+    return true
+  }
+  print("Restoring input source to: \(id)")
   let filter = [kTISPropertyInputSourceID!: id] as NSDictionary
   let inputSources =
     TISCreateInputSourceList(filter, false).takeUnretainedValue()
     as NSArray as! [TISInputSource]
+  guard !inputSources.isEmpty else {
+    return false
+  }
   let inputSource = inputSources[0]
   TISSelectInputSource(inputSource)
+  return true
 }
 
-// print(getInputSource())
-// setInputSource(to: "com.apple.keylayout.US")
+func getCurrentAppID() -> String? {
+  let currentApp = NSWorkspace.shared.frontmostApplication
+  return currentApp?.bundleIdentifier
+}
+
+let currentInputSourceObserver = NotificationCenter
+  .default
+  .publisher(for: NSTextInputContext.keyboardSelectionDidChangeNotification)
+  .map { _ in getInputSource() }
+  .removeDuplicates()
+  .sink { inputSource in
+    guard let currentApp = getCurrentAppID() else {
+      return
+    }
+
+    print("Switching to input source: \(inputSource)")
+    saveInputSource(inputSource, forApp: currentApp)
+  }
 
 let currentAppObserver = NSWorkspace
   .shared
   .notificationCenter
   .publisher(for: NSWorkspace.didActivateApplicationNotification)
   .sink { notification in
-    guard
-      let info =
-        notification.userInfo?[NSWorkspace.applicationUserInfoKey]
-        as? NSRunningApplication,
-      let identifier = info.bundleIdentifier
-    else {
+    guard let currentApp = getCurrentAppID() else {
       return
     }
 
-    // TODO: Remove this
-    print("Switching to app: \(identifier)")
+    print("Switching to app: \(currentApp)")
+    guard
+      let oldInputSource = userDefaults.string(forKey: currentApp),
+      setInputSource(to: oldInputSource)
+    else {
+      let newInputSource = getInputSource()
+      saveInputSource(newInputSource, forApp: currentApp)
+      return
+    }
   }
 
 CFRunLoopRun()
